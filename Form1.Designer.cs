96b6a7ba80083f9cdf8a5a2f8db71ccbef1f9ec1
NikitaOchin <NikitaOchinskiy@gmail.com>
2020-05-05 23:50:06 +0300

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,80 @@
+﻿namespace Chess
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.imageList1 = new System.Windows.Forms.ImageList(this.components);
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Dock = System.Windows.Forms.DockStyle.Right;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.Location = new System.Drawing.Point(809, 0);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 811);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // imageList1
+            // 
+            this.imageList1.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("imageList1.ImageStream")));
+            this.imageList1.TransparentColor = System.Drawing.Color.White;
+            this.imageList1.Images.SetKeyName(0, "download.jpg");
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(884, 811);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.ImageList imageList1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Chess
+{
+    public partial class Form1 : Form
+    {
+        Control[] Lb;
+        int n = 8;
+        int[] p = new int[2] { 0, 0 };
+        Color main_color = Color.Blue;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Lb = new Label[n*n];
+            
+            for(int i = 0; i < n; i++)
+                for(int j = 0; j < n; j++)
+                {
+                    Lb[i * n + j] = new Label();
+                    Lb[i * n + j].Parent = this;
+                    Lb[i * n + j].Visible = true;
+                    Lb[i * n + j].Size = new Size(100, 100);
+                    Lb[i * n + j].BackColor = main_color;
+                    Lb[i * n + j].Location = new Point(i * 100 + i, j * 100 + j);
+                    Lb[i * n + j].Click += new EventHandler(Click_label);
+                    Lb[i * n + j].Text = 0.ToString();
+                    Lb[i * n + j].Name = (i * n + j).ToString();
+                }
+        }
+
+        private void Click_label(object sender, EventArgs e)
+        {
+            Control l = sender as Label;
+            int m = Convert.ToInt32(l.Text);
+            for (int i = 0; i < n; i++)
+                for (int j = 0; j < n; j++)
+                {
+                    if (Lb[i * n + j].BackgroundImage != null)
+                    {
+                        Lb[i * n + j].BackgroundImage = null;
+                        Lb[i * n + j].BackColor = Color.Red;
+                    }
+                    Lb[i * n + j].Text = 0.ToString();
+                }
+            if (l.BackColor == main_color)
+                l.BackgroundImage = imageList1.Images[0];
+            else
+                l.BackColor = main_color;
+            for (int i = 0; i < n; i++)
+                for (int j = 0; j < n; j++)
+                {
+                    if (Lb[i * n + j].BackColor == Color.Red) continue;
+                    if (i + 2 >= 0 && j + 1 >= 0  && i + 2 < n && j + 1 < n && Lb[(i + 2) * n + (j + 1)].BackColor == main_color)
+                        Lb[i * n + j].Text = (Convert.ToInt32(Lb[i * n + j].Text) + 1).ToString();
+                    if (i + 2 >= 0 && j - 1 >= 0 && i + 2 < n && j - 1 < n && Lb[(i + 2) * n + (j - 1)].BackColor == main_color)
+                        Lb[i * n + j].Text = (Convert.ToInt32(Lb[i * n + j].Text) + 1).ToString();
+                    if (i + 1 >= 0 && j - 2 >= 0 && i + 1 < n && j - 2 < n && Lb[(i + 1) * n + (j - 2)].BackColor == main_color)
+                        Lb[i * n + j].Text = (Convert.ToInt32(Lb[i * n + j].Text) + 1).ToString();
+                    if (i + 1 >= 0 && j + 2 >= 0 && i + 1 < n && j + 2 < n && Lb[(i + 1) * n + (j + 2)].BackColor == main_color)
+                        Lb[i * n + j].Text = (Convert.ToInt32(Lb[i * n + j].Text) + 1).ToString();
+
+                    if (i - 2 >= 0 && j + 1 >= 0 && i - 2 < n && j + 1 < n && Lb[(i - 2) * n + (j + 1)].BackColor == main_color)
+                        Lb[i * n + j].Text = (Convert.ToInt32(Lb[i * n + j].Text) + 1).ToString();
+                    if (i - 2 >= 0 && j - 1 >= 0 && i - 2 < n && j - 1 < n && Lb[(i - 2) * n + (j - 1)].BackColor == main_color)
+                        Lb[i * n + j].Text = (Convert.ToInt32(Lb[i * n + j].Text) + 1).ToString();
+                    if (i - 1 >= 0 && j - 2 >= 0 && i - 1 < n && j - 2 < n && Lb[(i - 1) * n + (j - 2)].BackColor == main_color)
+                        Lb[i * n + j].Text = (Convert.ToInt32(Lb[i * n + j].Text) + 1).ToString();
+                    if (i - 1 >= 0 && j + 2 >= 0 && i - 1 < n && j + 2 < n && Lb[(i - 1) * n + (j + 2)].BackColor == main_color)
+                        Lb[i * n + j].Text = (Convert.ToInt32(Lb[i * n + j].Text) + 1).ToString();
+                }
+            timer1.Enabled = true;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Random r = new Random();
+            p[0] = r.Next(0, n - 1);
+            p[1] = r.Next(0, n - 1);
+            foreach (Label l in Lb)
+                l.BackColor = main_color;
+            Click_label(Lb[p[0] * n + p[1]], new EventArgs());
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            int min = 8;
+            int[] np = new int[2] { 0, 0 };
+            
+            if (p[0] + 2 >= 0 && p[1] + 1 >= 0 && p[0] + 2 < n && p[1] + 1 < n && Lb[(p[0] + 2) * n + (p[1] + 1)].BackColor == main_color && Convert.ToInt32(Lb[(p[0] + 2) * n + (p[1] + 1)].Text)<=min)
+            {
+                min = Convert.ToInt32(Lb[(p[0] + 2) * n + (p[1] + 1)].Text); np[0] = p[0] + 2; np[1] = p[1] + 1;
+            }
+            if (p[0] + 2 >= 0 && p[1] - 1 >= 0 && p[0] + 2 < n && p[1] - 1 < n && Lb[(p[0] + 2) * n + (p[1] - 1)].BackColor == main_color && Convert.ToInt32(Lb[(p[0] + 2) * n + (p[1] - 1)].Text) <= min)
+            {
+                min = Convert.ToInt32(Lb[(p[0] + 2) * n + (p[1] - 1)].Text); np[0] = p[0] + 2; np[1] = p[1] - 1;
+            }
+            if (p[0] + 1 >= 0 && p[1] - 2 >= 0 && p[0] + 1 < n && p[1] - 2 < n && Lb[(p[0] + 1) * n + (p[1] - 2)].BackColor == main_color && Convert.ToInt32(Lb[(p[0] + 1) * n + (p[1] - 2)].Text) <= min)
+            {
+                min = Convert.ToInt32(Lb[(p[0] + 1) * n + (p[1] - 2)].Text); np[0] = p[0] + 1; np[1] = p[1] - 2;
+            }
+            if (p[0] + 1 >= 0 && p[1] + 2 >= 0 && p[0] + 1 < n && p[1] + 2 < n && Lb[(p[0] + 1) * n + (p[1] + 2)].BackColor == main_color && Convert.ToInt32(Lb[(p[0] + 1) * n + (p[1] + 2)].Text) <= min)
+            {
+                min = Convert.ToInt32(Lb[(p[0] + 1) * n + (p[1] + 2)].Text); np[0] = p[0] + 1; np[1] = p[1] + 2;
+            }
+            if (p[0] - 2 >= 0 && p[1] + 1 >= 0 && p[0] - 2 < n && p[1] + 1 < n && Lb[(p[0] - 2) * n + (p[1] + 1)].BackColor == main_color && Convert.ToInt32(Lb[(p[0] - 2) * n + (p[1] + 1)].Text) <= min)
+            {
+                min = Convert.ToInt32(Lb[(p[0] - 2) * n + (p[1] + 1)].Text); np[0] = p[0] - 2; np[1] = p[1] + 1;
+            }
+            if (p[0] - 2 >= 0 && p[1] - 1 >= 0 && p[0] - 2 < n && p[1] - 1 < n && Lb[(p[0] - 2) * n + (p[1] - 1)].BackColor == main_color && Convert.ToInt32(Lb[(p[0] - 2) * n + (p[1] - 1)].Text) <= min)
+            {
+                min = Convert.ToInt32(Lb[(p[0] - 2) * n + (p[1] - 1)].Text); np[0] = p[0] - 2; np[1] = p[1] - 1;
+            }
+            if (p[0] - 1 >= 0 && p[1] - 2 >= 0 && p[0] - 1 < n && p[1] - 2 < n && Lb[(p[0] - 1) * n + (p[1] - 2)].BackColor == main_color && Convert.ToInt32(Lb[(p[0] - 1) * n + (p[1] - 2)].Text) <= min)
+            {
+                min = Convert.ToInt32(Lb[(p[0] - 1) * n + (p[1] - 2)].Text); np[0] = p[0] - 1; np[1] = p[1] - 2;
+            }
+            if (p[0] - 1 >= 0 && p[1] + 2 >= 0 && p[0] - 1 < n && p[1] + 2 < n && Lb[(p[0] - 1) * n + (p[1] + 2)].BackColor == main_color && Convert.ToInt32(Lb[(p[0] - 1) * n + (p[1] + 2)].Text) <= min)
+            {
+                min = Convert.ToInt32(Lb[(p[0] - 1) * n + (p[1] + 2)].Text); np[0] = p[0] - 1; np[1] = p[1] + 2;
+            }
+            if (p[0] == np[0] && p[1] == np[1]) timer1.Enabled = false;
+            p[0] = np[0]; p[1] = np[1];
+            Click_label(Lb[p[0] * n + p[1]], new EventArgs());
+        }
+    }
+}
